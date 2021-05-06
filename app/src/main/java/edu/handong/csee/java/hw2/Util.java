5bcd85273be6db1cc48cccfad7c937f3162ad95c
Yun <Yun@DESKTOP-RCKNO9Q>
2021-05-06 16:27:06 +0900

    Commit for IntegratedConverter

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,17 @@
+package edu.handong.csee.java.hw2;
+
+/**
+ * This class capitalize every characters in stringValue.
+ * @author YunSeok Jeong
+ */
+public class Util {
+    /**
+     * This method capitalize every characters in stringValue.
+     * @param stringValue
+     * @return stringValue.toUpperCase()
+     */
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
